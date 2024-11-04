04a7b877b0e500d3d854578dc7fe654bc1202785
danieldemesa <danieldemesa15@gmail.com>
2024-11-03 19:18:22 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const studentDetails = {
+    studentID: '101440281',
+    name: 'Daniel De Mesa-Borrett',
+    college: 'George Brown College, Toronto'
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" 
+          className="App-logo" 
+          alt="React logo" 
+        />
+        <h1>Welcome to Fullstack Development - I</h1>
+        <h2>React JS Programming Week09 Lab Exercise</h2>
+        <div className="student-info">
+          <p>{studentDetails.studentID}</p>
+          <p>{studentDetails.name}</p>
+          <p>{studentDetails.college}</p>
+        </div>
       </header>
     </div>
   );
